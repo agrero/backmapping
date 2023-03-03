472d921b0e8339e649a339b3094a26b5b18dd8d5
agrero <86486604+agrero@users.noreply.github.com>
2023-03-03 09:56:56 -0800

    added bash script writing from the dict

--- write_lammps.py
+++ write_lammps.py
@@ -1,16 +1,11 @@
-def write_lammps_config(config_list, filename):
-    #can probably rewrite this as a dictionary
-    with open(filename, 'w') as f:
-        for i in config_list:
-            f.write(f'{i}\n')
+from backpack import writepack as wp
 
 #######
-# Parameters
+# Parameters for writing lammps config
 #######
-# should likely distinguish between if you're reading a restart 
-# or you're reading an input file
+
 # need a way to change the mass in here CANNNOT forget to do that 
-param_dict ={
+peequil_dict ={
     'atom_style' : 'molecular', 
     'boundary': 'p p p',
     'units' : 'real',
@@ -26,13 +21,49 @@ param_dict ={
     'mass' : '* 14.0',
     'pair_style' : 'lj/mdf 12.0 14.0',
     'pair_coeff' : '1 1 0 0.0912 2.95 12.0 14.0',
-    'neigh_modify' : 'every 1 delay 0 cehck yes',
+    'neigh_modify' : 'every 1 delay 0 check yes',
+    'minimize' : '1.0e-4 1.0e-6 1000 1000',
     'thermo_style' : 'custom step temp press ke pe etotal',
     'thermo_modify' : 'lost warn',
     'dump' : 'coords all atom 1 dump.xyz.dat',
     'dump_modify' : 'coords',
     'restart' : '10000 restart.dat'
 } 
+
+soft_dict = {
+    'atom_style' : 'molecular',
+    'boundary' : 'p p p',
+    'units' : 'real',
+    'read_data' : 'lammps-AT-config',
+    'bond_style' : 'harmonic',
+    'bond_coef' : '1 450 1.54',
+    'special_bonds' : 'lj 0.0 0.0 0.0 angle no dihedral no',
+    'angle_style' : 'harmonic',
+    'angle_coeff' : '1 450 1.54',
+    'dihedral_style' : 'opls',
+    'dihedral_coeff' : '* 1.14110 -0.27084 3.143 0.0',
+    'mass' : '* 14.0',
+    'pair_style' : 'soft 10.5',
+    'pair_coeff' : '* * 10.0',
+    'variable' : 'prefactor equal ramp(10,100)',
+    'fix' : '1 all adapt 1 pair soft a * * v_prefactor',
+    'neighbor' : '0.4 bin',
+    'neigh_modify' : 'every 1 delay 0 check yes',
+    'thermo_style' : 'custom step temp press ke pe etotal',
+    'thermo_modify' : 'lost warn',
+    'thermo' : '1000',
+    'dump' : 'coords all atom 1 dump.xyz.dat',
+    'dump_modify' : 'coords scale no',
+}
+# maybe later we try to do the run nos and timesteps like we did the beginning and end things 
+# in the backmapping protocol writing
+wp.write_lammps_config(peequil_dict,filename='in.PEequil', restart=True, 
+                    timesteps=[0.025,0.08,0.25,0.5,0.75,1.0,1.25], 
+                    run_nos=[100,100,100,100,100,100,20000])
+
+#replace none when you get the chance
+wp.write_lammps_config(soft_dict, filename='in.PEsoft', restart=False, 
+                       timesteps=[1.00], run_nos=[20000])
 # NOT INCLUDED
     # minimize
     # run
@@ -40,68 +71,32 @@ param_dict ={
 # this is how it should write the parameters
 # we should also not forget to ask if you'd want to write the data at each step
 # in order to make a video!
-for key in param_dict:
-    print(f'{key} {param_dict[key]}')
-
-atom_style = 'atom_style molecular'
-boundary = 'boundary p p p'
-units = 'units real'
-
-read_data = 'read_data lammps-AT-config'
-read_restart = 'read_restart RESTART'
-
-bond_style = 'bond_style harmonic'
-bond_coeff = 'bond_coeff 1 450 1.54'
-special_bonds = 'special_bonds lj 0.0 0.0 0.0 angle no dihedral no'
-
-angle_style = 'angle_style harmonic'
-angle_coeff = 'angle_coeff 1 61.875 114.00'
-
-dihedral_style = 'dihedral_style opls'
-dihedral_coeff = 'dihedral_coeff * 1.14110 -0.27084 3.143 0.0'
-
-mass = 'mass * 14.0'
-
-pair_style = 'pair_style lj/mdf 12.0 14.0'
-pair_coeff = 'pair_coeff 1 1 0.0912 3.95 12.0 14.0'
-
-neigh_modify = 'neigh_modify every 1 delay 0 check yes'
-minimize = 'minimize 1.0e-4 1.0e-6 1000 1000'
-
-thermo_style = 'thermo_style custom step temp press ke pe etotal'
-thermo_modify = 'thermo_modify lost warn'
-
-dump = 'dump coords all atom 1 dump.xyz.dat'
-dump_modify = 'dump_modify coords scale no'
-
-restart = 'restart 10000 restart.dat'
-
 #######
 # Run Conditions
 #######
 
-timestep = 'timestep 0.02'
-run = 'run 100'
 #ask if you wanna minimize between each run
 # minimize is the same for each
 # i think the beginning parameters can be put in a dict but the run 
 # should be written through a function
 
-timestep_2 = 'timestep 0.75'
-run_2 = 'run 100'
-#minimize
+#######
+# Inputs for writing workflow bash
+#######
 
-timestep_3 = 'timestep 1.0'
-run_3 = 'run 100'
-#minimize
+workflow_dict = {
+    'job-name' : '"N10MDFrho0315"', # the second parentheses are important! 
+    'output' : '"N10.%j.%N.out"',
+    'partition' : 'short',
+    'time' : '0-01:00:00',
+    'error': 'joberr',
+    'nodes' : '1',
+    'ntasks-per-node' : '5',
+    'cpus-per-task' : '1',
+    'export' : 'ALL',
+    'mail-user' : 'email@address.lll',
+    'mail-type' : ['begin', 'end'],
+    'account' : 'guenzagrp'
+}
 
-timestep_4 = 'timestep 1.25'
-run_4 = 'run 20000'
-
-write_restart = 'write_restart FINALCONFIG'
-stuff_list = [atom_style, boundary, units, read_restart, bond_style, bond_coeff, special_bonds,
-              angle_style, angle_coeff, dihedral_style, dihedral_coeff, mass, pair_style,
-              pair_coeff, neigh_modify, thermo_style, thermo_modify, dump, dump_modify, restart, timestep,run, minimize,
-              timestep_2, run_2, minimize,timestep_3, run_3, minimize,timestep_4, run_4, write_restart]
-
-#write_lammps_config(stuff_list, 'lammps_equil_config')
+wp.write_backmapping_protocol(filename='protocol.sh', head_dict=workflow_dict)
